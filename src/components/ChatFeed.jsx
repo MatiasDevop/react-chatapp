a7a8191f75afd8e047e8c5c0c66ea37bf10aa201
Nestor Matias <solomatias321@gmail.com>
2021-08-10 11:05:16 +0200

    adding types of  messages and show the individually

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -16,16 +16,44 @@ const ChatFeed = (props) => {
             const message = messages[key];
             const lastMessageKey = index === 0 ? null: keys[index - 1];
             const isMyMessage = userName === message.sender.username;
+
+            return (
+                <div key={`msg_${index}`} style={{ width: '100px' }}>
+                    <div className="message-block">
+                        {
+                            isMyMessage
+                            ?<MyMessage  message={message}/>
+                            :<TheirMessage message={message} lastMessage={messages[lastMessageKey]} />
+                        }
+                    </div>
+                    <div className="read-recipts" style={{ marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px'}}>
+                        read-receipts
+                    </div>
+                </div>
+            );
         })
     }
 
-    renderMessages()
+    //renderMessages()
+
+    if (!chat) return 'Loading ...';
 
     return (
-        <div key={`msg_${index}`} style={{ width: '100px' }}>
-            
+        <div className="chat-feed">
+            <div className="chat-title-container">
+                <div className="chat-title">{chat?.title}</div>
+                <div className="chat-subtitle">
+                    {chat.people.map((person) => `${person.person.username}`)}
+                </div>
+            </div>
+            {renderMessages()}
+            <div style={{ height: '100px'}} />
+            <div className="message-form-container">
+                <MessageForm {...props} chatId={activeChat} />
+            </div>
         </div>
-    )
+    );
+   
 }
 
 export default ChatFeed;
